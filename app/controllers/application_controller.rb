b0cfab849f108b0964c0e0e1109a17fd31554eba
Michael Ho <chacharoti@gmail.com>
2019-08-17 22:51:45 -0700

    Permit account_update params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
   protect_from_forgery
   def home
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name, :avatar, :phone_number, :address])
   end
 end
