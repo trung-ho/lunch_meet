151ba8f15fea69389f4d2350090eeaf222f6e28a
Michael Ho <chacharoti@gmail.com>
2019-09-05 20:14:10 -0700

    Add Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+require 'faker'
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+category_ids = Category.all.pluck(:id)
+
+(1..100).each do |count|
+  user = User.create email: Faker::Internet.safe_email, password: 'password', first_name: Faker::Name.first_name , last_name: Faker::Name.last_name 
+  category_ids.sample(3).each do |category_id|
+    user.user_categories.create category_id: category_id
+  end
+end
