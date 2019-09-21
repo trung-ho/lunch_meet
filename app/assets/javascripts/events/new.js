700e41a8d466e52be5ac77ec524cad196d9407b4
Michael Ho <chacharoti@gmail.com>
2019-09-21 00:26:52 -0700

    Add min date for datetimepicker

--- app/assets/javascripts/events/new.js
+++ app/assets/javascripts/events/new.js
@@ -2,7 +2,8 @@ $(function () {
   if($("#event-new-page").length > 0){
     $('#start_at_datetime_picker').datetimepicker({
       sideBySide: true,
-      format: 'YYYY-MM-DD HH:mm'
+      format: 'YYYY-MM-DD HH:mm',
+      minDate: moment()
     });
 
     document.getElementById('event_radius').oninput = function(){
